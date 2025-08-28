9efe78b4839b9e8de3a6b513f7224ccde8bcfab1
Tanjil Hassan <kenk14382@gmail.com>
2025-08-29 00:37:08 +0600

    main section and history section done

--- app.js
+++ app.js
@@ -1,1 +1,130 @@
-console.log("working");
+const services = [
+  {
+    id: 1,
+    img: "./assets/emergency.png",
+    serviceTitle: "National Emergency Number",
+    serviceType: "National Emergency",
+    phone: "999",
+    category: "All",
+  },
+  {
+    id: 2,
+    img: "/assets/police.png",
+    serviceTitle: "Police Helpline Number",
+    serviceType: "Police",
+    phone: "999",
+    category: "Police",
+  },
+  {
+    id: 3,
+    img: "/assets/fire-service.png",
+    serviceTitle: "Fire Service Number",
+    serviceType: "Fire Service",
+    phone: "999",
+    category: "Fire ",
+  },
+  {
+    id: 4,
+    img: "/assets/emergency.png",
+    serviceTitle: "Ambulance Service",
+    serviceType: "Ambulance",
+    phone: "1994-999999",
+    category: "Health",
+  },
+  {
+    id: 5,
+    img: "/assets/emergency.png",
+    serviceTitle: "Women & Child Helpline",
+    serviceType: "Women & Child Helpline",
+    phone: "109",
+    category: "Help",
+  },
+  {
+    id: 6,
+    img: "/assets/emergency.png",
+    serviceTitle: "Anti-Corruption Helpline",
+    serviceType: "Anti-Corruption",
+    phone: "106",
+    category: "Govt.",
+  },
+  {
+    id: 7,
+    img: "/assets/emergency.png",
+    serviceTitle: "Electricity Helpline",
+    serviceType: "Electricity Outage",
+    phone: "16216",
+    category: "Electricity",
+  },
+  {
+    id: 8,
+    img: "/assets/emergency.png",
+    serviceTitle: "Brac Helpline",
+    serviceType: "Brac",
+    phone: "16445",
+    category: "NGO",
+  },
+  {
+    id: 9,
+    img: "/assets/emergency.png",
+    serviceTitle: "Bangladesh Railway Helpline ",
+    serviceType: "Bangladesh Railway",
+    phone: "163",
+    category: "Travel",
+  },
+];
+
+const cardContainer = document.getElementById("cardContainer");
+const callHistoryContainer = document.getElementById("callHistoryContainer");
+
+services.forEach(service => {
+  const card = document.createElement("div");
+  card.classList.add("space-y-3", "py-3", "px-4", "rounded-lg", "shadow-xl/20");
+  card.innerHTML = `
+  <div class="flex justify-between items-center">
+    <div class="p-3 rounded-[16px] bg-[#FFE3E2] w-[50px] h-[50px]">
+      <img src=${service.img} class="w-full h-full" alt="">
+    </div>
+    <i id="reactBtn" class="fa-regular fa-heart cursor-pointer"></i>
+  </div>
+  <p class="text-lg font-bold">${service.serviceTitle}</p>
+  <p class="text-gray-500">${service.serviceType}</p>
+  <p class="text-2xl font-bold">${service.phone}</p>
+  <span class="bg-gray-200 text-gray-600 px-4 py-1 rounded-2xl">${service.category}</span>
+  <div class="mt-5 w-full flex justify-center items-center gap-3">
+    <button class="border border-gray-300 hover:bg-gray-300  w-[50%] rounded-md py-2 text-gray-600 cursor-pointer" data-copyBtn=${service.id}><i class="fa-solid fa-copy"></i> Copy</button>
+    <button class="bg-[#00A63E] hover:bg-[#009135] text-white  w-[50%] py-2 rounded-md cursor-pointer" data-id=${service.id}><i class="fa-solid fa-phone"></i> Call</button>
+  </div>
+  `;
+
+  cardContainer.appendChild(card)
+  
+})
+
+cardContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "BUTTON") {
+    
+    const id = e.target.getAttribute("data-id")
+    
+    let singleService = services.find(service => service.id == id)
+
+    const callHistoryCard = document.createElement("div")
+    callHistoryCard.classList.add("flex", "justify-between", "items-center", "bg-gray-200", "p-3", "rounded-xl")
+
+    callHistoryCard.innerHTML = `
+    <div>
+    <p class="font-bold">${singleService.serviceTitle}</p>
+    <p class="text-gray-400">${singleService.phone}</p>
+    </div>
+    <p>${new Date().toLocaleTimeString()}</p>
+    `
+    callHistoryContainer.appendChild(callHistoryCard)
+
+    
+  }
+  else if (e.target.tagName === "I") {
+    let reactCount = document.getElementById("reactCount");
+    reactCount.innerText = Number(reactCount.innerText) + 1;
+  }
+  
+})
+
